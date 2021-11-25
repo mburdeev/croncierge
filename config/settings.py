b0c5cd47b398af15c2d5b3c9d80862263a6f35ac
Maxim B <maxim.by@yandex.ru>
2021-11-26 00:33:46 +0300

    fix: celery work, use postgres db

--- config/settings.py
+++ config/settings.py
@@ -94,10 +94,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600),
 }
 
 
