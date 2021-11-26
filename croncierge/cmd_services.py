aea7a1384b3ca6b13dba157e8fdfd6a0f79bf49b
Maxim B <maxim.by@yandex.ru>
2021-11-26 21:20:29 +0300

    feat: add start, exit datetime to cmd run

--- croncierge/cmd_services.py
+++ croncierge/cmd_services.py
@@ -1,5 +1,7 @@
-import subprocess
 import shlex
+import subprocess
+from datetime import datetime
+
 
 def revise_cmd(cmd) -> list:
     if isinstance(cmd, str):
@@ -12,21 +14,24 @@ def revise_cmd(cmd) -> list:
 
 class CommandResponse():
 
-    def __init__(self, stdout, stderr, status) -> None:
+    def __init__(self, stdout, stderr, status, started_at, exited_at):
         self.stdout: str = stdout
         self.stderr: str = stderr
         self.status: int = status
+        self.started_at: datetime = started_at
+        self.exited_at: datetime = exited_at
 
     def __str__(self) -> str:
         return  "\n\n".join(
             f"{label}:\n{data}" for label, data in zip(
-                ['status', 'stdout', 'stderr'],
-                [self.status, self.stdout, self.stderr]
+                ['status', 'stdout', 'stderr', 'started at', 'exited at'],
+                [self.status, self.stdout, self.stderr, self.started_at, self.exited_at]
             ))
 
 
 def run_cmd(cmd, timeout=None):
     cmd = revise_cmd(cmd)
+    started_at = datetime.now()
     process = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -34,10 +39,13 @@ def run_cmd(cmd, timeout=None):
         encoding='utf8')
 
     process.wait(timeout=timeout)
+    exited_at = datetime.now()
 
     return CommandResponse(
         stdout=process.stdout.read(),
         stderr=process.stderr.read(),
-        status=process.returncode
+        status=process.returncode,
+        started_at=started_at,
+        exited_at=exited_at,
     )
     
